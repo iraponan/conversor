a4effc7f94620f18e8ac383aa7e0adb97822b82e
Iraponan Marinho <iraponan.aires@gmail.com>
2024-01-23 22:00:14 -0300

    Incluindo as rotinas FutureBuilder.

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,6 @@ import 'dart:convert';
 import 'package:conversor/pages/home.dart';
 import 'package:flutter/material.dart';
 
-import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
-import 'package:convert/convert.dart';
-
-const String key = String.fromEnvironment('KEYHGBRASIL');
-final request = Uri.https('api.hgbrasil.com', '/finance/quotations', {'key' : key});
-
 void main() async {
   runApp(
     const MaterialApp(
@@ -17,9 +10,4 @@ void main() async {
       home: Home(),
     ),
   );
-}
-
-Future<Map> getData() async {
-  http.Response response = await http.get(request);
-  return json.decode(response.body);
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,15 @@
+import 'dart:convert';
+
+import 'package:conversor/main.dart';
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'package:convert/convert.dart';
+
+const String key = String.fromEnvironment('KEYHGBRASIL');
+final request =
+    Uri.https('api.hgbrasil.com', '/finance/quotations', {'key': key});
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,11 +24,51 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('\$ Conversor \$'),
+        title: const Text('\$ Conversor \$'),
         backgroundColor: Colors.amber,
         centerTitle: true,
       ),
-      body: ,
+      body: FutureBuilder<Map>(
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return const Center(
+                child: Text(
+                  'Carregando Dados...',
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 25,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if (snapshot.hasError) {
+                return const Center(
+                  child: Text(
+                    'Erro ao Carregar os Dados!!! :(',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 25,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Container(
+                  color: Colors.green,
+                );
+              }
+          }
+        },
+        future: getData(),
+      ),
     );
   }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
